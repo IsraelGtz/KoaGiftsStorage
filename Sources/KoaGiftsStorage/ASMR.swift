7161f79759e6ccfd3ad98748d1b4d1ad891a38f6
Israel Gutierrez <igtz.castillo@gmail.com>
2026-08-31 03:03:16 -0600

    Update id of ASMR and Letter to be initialized during the creation of the object
    - let id = UUID()

--- Sources/KoaGiftsStorage/ASMR.swift
+++ Sources/KoaGiftsStorage/ASMR.swift
@@ -8,15 +8,8 @@
 import Foundation
 
 public struct ASMR: Codable, Identifiable, Hashable {
-    public let id: UUID
+    public let id = UUID()
     public let name: String
     public let title: String
     public let audioName: String
-
-    init(name: String, title: String, audioName: String) {
-        id = UUID()
-        self.name = name
-        self.title = title
-        self.audioName = audioName
-    }
 }
--- Sources/KoaGiftsStorage/Letter.swift
+++ Sources/KoaGiftsStorage/Letter.swift
@@ -8,19 +8,10 @@
 import Foundation
 
 public struct Letter: Codable, Identifiable, Hashable {
-    public let id: UUID
+    public let id: UUID = UUID()
     public let name: String
     public let title: String
     public let body: String
     public let imageName: String?
     public let audioName: String?
-
-    init(name: String, title: String, body: String, imageName: String?, audioName: String?) {
-        id = UUID()
-        self.name = name
-        self.title = title
-        self.body = body
-        self.imageName = imageName
-        self.audioName = audioName
-    }
 }
